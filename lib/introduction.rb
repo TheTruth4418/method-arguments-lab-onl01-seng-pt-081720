acf353dde231a89b34b10d41efa65e846550c499
William West <wwest466@gmail.com>
2020-08-17 15:26:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 def introduction(name)
-  "Hi, my name is #{name}.\n"
+  puts "Hi, my name is #{name}.\n"
 end
 
 def introduction_with_language(name, language)
-  "Hi, my name is #{name} and I am learning how to program in #{language}.\n"
+  puts "Hi, my name is #{name} and I am learning how to program in #{language}.\n"
 end
 
-introduction("Josh")
+
